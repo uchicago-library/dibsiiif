055cdc9c9742d5ca758bc24fe94f291ed0eafc6a
Matt Teichman <teichman@uchicago.edu>
2023-01-18 10:22:20 -0600

    update iiifify.sh to process the oldest entry in the status files dir each time it is run

--- iiifify.sh
+++ iiifify.sh
@@ -10,12 +10,35 @@
 # USAGE:
 # /bin/bash /path/to/this/script.sh
 
-# set the nullglob in case there are no `*-initiated` files
-shopt -s nullglob
+## Not using glob anymore so this is unnecessary
+## # set the nullglob in case there are no `*-initiated` files
+## shopt -s nullglob
+
+LOGDIR=/data/local/logs
+SENDER=root@dibs.lib.uchicago.edu
+TeamsEmail="dibsiiif_logs - Controlled Digital Lending <c2de5ca3.teams.uchicago.edu@amer.teams.ms>"
+RECIPIENT="$TeamsEmail, Matt Teichman <teichman@lib.uchicago.edu>"
+
+DIR=$(dirname "$0")
+INI=$DIR/iiifify.ini
+STATUS_FILES_DIR=$(source "$INI" && echo "$STATUS_FILES_DIR")
+PYTHON=$(source "$INI" && echo "$PYTHON")
 
 # expecting an absolute path as an argument
-for FILE in "$(source "$(dirname "$0")"/iiifify.ini && echo "$STATUS_FILES_DIR")"/*-initiated; do
+## for FILE in "$(source "$(dirname "$0")"/iiifify.ini && echo "$STATUS_FILES_DIR")"/*-initiated; do
+#  Only process one file so we don't run into a race condition with cron.
+#  Use ls -rt to get the oldest entry (i.e., the first checkbox selected by staff)
+for FILE in $(ls -tr $STATUS_FILES_DIR/ | grep '^.*-initiated$' | head -1) ; do
     barcode=$(basename "$FILE" | cut -d '-' -f 1)
-    python=$(source "$(dirname "$0")"/iiifify.ini && echo "$PYTHON")
-    $python "$(dirname "$0")"/dibsiiif.py "$barcode"
+    TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
+    OUTFILE="$LOGDIR/dibsiiif_out_${barcode}_${TIMESTAMP}.txt"
+    ## echo "$PYTHON $DIR/dibsiiif.py \"$barcode\" > $OUTFILE 2>&1"
+    $PYTHON $DIR/dibsiiif.py "$barcode" > $OUTFILE 2>&1
+    if [ -s "$OUTFILE" ]; then
+        SUBJECT="dibsiiif error on barcode: $barcode"
+        ## cat $OUTFILE | mail -s "${SUBJECT}" "${RECIPIENT}" -sendmail-option -f${SENDER}
+        cat $OUTFILE | /host/bin/cronmail -s "${SUBJECT}" "${RECIPIENT}"
+    else
+        rm "$OUTFILE"
+    fi
 done
